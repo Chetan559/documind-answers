3a616673cfca62613fecb9b2219a3f8986111e08
Chetan Sharma <cschetan559@gmail.com>
2026-03-06 11:28:58 +0530

    cursor bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/documind-answers/",
+  // base: "/documind-answers/",
   server: {
     host: "::",
     hmr: {
